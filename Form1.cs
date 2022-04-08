6469821cd9f1f6a1db3cb974faace64d077a3cdf
Сергей Ширин <default-sl@yandex.ru>
2022-04-08 09:51:04 +0700

    add files opening

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,8 @@ namespace combining_translations
 {
     public partial class Form1 : Form
     {
-        //private string ruFileName;
+        private string ruFileName;
+        private string enFileName;
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +21,31 @@ namespace combining_translations
 
         private void button1_Click(object sender, EventArgs e)
         {
+            openFileDialog1.Filter = "Файл перевода 'lang_ru.js' | lang_ru.js";
 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ruFileName = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = ruFileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Filter = "Файл перевода 'lang_en.js' | lang_en.js";
+
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                enFileName = openFileDialog2.FileName;
+                textBox2.Clear();
+                textBox2.Text = enFileName;
+            }
         }
     }
 }
